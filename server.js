72ce3f0ab17bf669e03ef951fcd85d34fd6b1c1a
RamyEl-basyouni <ramy93.ali@gmail.com>
2019-05-11 00:00:38 +0200

    fixing dist folder mapping error

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular-graphic-editor'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/angular-graphic-editor/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
